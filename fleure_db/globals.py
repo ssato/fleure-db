34118690ff5422d5dcd47c3e49ca8bc66b105a01
Satoru SATOH <satoru.satoh@gmail.com>
2017-03-10 02:45:46 +0900

    add

---
+++ fleure_db/globals.py
@@ -0,0 +1,15 @@
+#
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017 Satoru SATOH <ssato@redhat.com>
+# Copyright (C) 2017 Red Hat, Inc.
+# License: MIT
+#
+"""fleure DB - globals
+"""
+from __future__ import absolute_import
+
+
+CONF_PATH = "/etc/fleure/db.d/*.yml"  # Glob paths pattern
+
+# vim:sw=4:ts=4:et:
