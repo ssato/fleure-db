d59e6fd38290ccec5d7b421901065cb299ac178b
Satoru SATOH <satoru.satoh@gmail.com>
2017-03-10 13:29:01 +0900

    enhancement: add an utility function .utils.gen_id_for_values to get ID (int value) for values somehow

--- fleure_db/utils.py
+++ fleure_db/utils.py
@@ -10,6 +10,7 @@
 from __future__ import absolute_import
 
 import datetime
+import hashlib
 import itertools
 import os.path
 import subprocess
@@ -57,6 +58,44 @@ def sgroupby(items, kfn, kfn2=None):
     """
     return (list(g) if kfn2 is None else sorted(g, key=kfn2) for _k, g
             in itertools.groupby(sorted(items, key=kfn), kfn))
+
+
+_C_DIGITS = "0 1 2 3 4 5 6 7 8 9".split()
+_H2I_BASE = ord('a') - len(_C_DIGITS) - 1  # needed to skip digits.
+
+
+def _c2i(char):
+    """
+    :param char: a character of [:alnum:]
+    :return: force-converted int from given `char`
+
+    >>> (_c2i('0'), _c2i('1'), _c2i('9'))
+    (0, 1, 9)
+    >>> _c2i('a')
+    11
+    >>> _c2i('z')
+    36
+    """
+    return int(char) if char in _C_DIGITS else ord(char) - _H2I_BASE
+
+
+def _hex2int(hstr):
+    """Force to convert a hexdigest string `hstr` to int.
+
+    :param hstr: Hexdigest string only consists of chars of [:alnum:]
+    """
+    return int(''.join("{:0>2}".format(_c2i(c)) for c in hstr))
+
+
+def gen_id_for_values(values):
+    """
+    :param values: List of objct can become str
+    :return: Integer represents 'ID' value to identify `values` somehow
+    """
+    # How long it should be?
+    vals_s = ' '.join(str(v).encode('utf-8') for v in values)
+    digest = hashlib.sha256(vals_s.encode('utf8')).hexdigest()
+    return _hex2int(digest[:8])
 
 
 def is_dnf_available():
